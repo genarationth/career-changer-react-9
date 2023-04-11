5d748df06f67f2281d5927f41194deb0ee436444
Wipoo Suksumek <124798841+wipoosuk@users.noreply.github.com>
2023-04-11 14:17:12 +0700

    loop react

--- 05-react-hook-useeffect/01-useEffect/04-exercise/src/main.jsx
+++ 05-react-hook-useeffect/01-useEffect/04-exercise/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/style/index.css'
+import AutoSync from './AutoSync'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // <App />
+  <AutoSync />
+
 )
