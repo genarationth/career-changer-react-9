2d7dbe777e6334fb78128d4b7837800cfc962df3
Wipoo Suksumek <124798841+wipoosuk@users.noreply.github.com>
2023-04-11 10:55:53 +0700

    first commit

---
+++ 05-react-hook-useeffect/01-useEffect/03-exercise/src/AutoSync.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+const App = () => {
+  const [rate, setRate] = useState(0);
+  const [exchange, setExchange] = useState(1);
+
+  const syncRate = () => {
+    setRate(30);
+  };
+
+  const handleExchangeChange = (event) => {
+    const newExchange = event.target.value;
+    setExchange(newExchange);
+  };
+
+  useEffect(syncRate, [])
+
+  return (
+    <div>
+      <h1>Current Rate: {rate}</h1>
+      <input type="number" value={exchange} onChange={handleExchangeChange} />
+      <h2>Calculated Exchange: {exchange * rate}</h2>
+    </div>
+  );
+};
+
+export default App;
--- 05-react-hook-useeffect/01-useEffect/03-exercise/src/main.jsx
+++ 05-react-hook-useeffect/01-useEffect/03-exercise/src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/style/index.css'
+import AutoSync from './Autosync'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  // <App />
+  <AutoSync />
 )
