a3b6200736ef568ce68d7eb69f6f00583447b194
Wipoo Suksumek <124798841+wipoosuk@users.noreply.github.com>
2023-04-11 10:56:57 +0700

    changeuppercase

--- 05-react-hook-useeffect/01-useEffect/03-exercise/src/main.jsx
+++ 05-react-hook-useeffect/01-useEffect/03-exercise/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/style/index.css'
-import AutoSync from './Autosync'
+import AutoSync from './AutoSync'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <App />
